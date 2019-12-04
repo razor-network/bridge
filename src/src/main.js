b431ff6e7e7c0dc195bd6f3d7820277e0e5a390f
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-12-04 16:38:11 +0700

    add skale ui

--- src/src/main.js
+++ src/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Skale from './Skale.vue'
 
 Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+console.log('lol',process.env.VUE_APP_NETWORK)
+if (process.env.VUE_APP_NETWORK == "matic") {
+    new Vue({
+        render: h => h(App),
+    }).$mount('#app')
+} else if (process.env.VUE_APP_NETWORK == "skale") {
+    new Vue({
+        render: h => h(Skale),
+    }).$mount('#app')
+}
