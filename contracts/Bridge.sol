f26d5644257ebf44d76dfe7f2d2074711e706186
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-12-05 19:34:59 +0700

    addjob and getjob

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -3,7 +3,15 @@ pragma solidity 0.5.10;
 
 contract Bridge {
 
-    mapping (uint256 => uint256) results;
+    struct Job {
+        string url;
+        string selector;
+        string name;
+        uint256 result;
+    }
+
+    mapping (uint256 => uint256) public results;
+    mapping (uint256 => Job) public jobs;
     address public owner = msg.sender;
 
     function setResult(uint256 id, uint256 result) external {
@@ -11,7 +19,17 @@ contract Bridge {
         results[id] = result;
     }
 
+    function setJob(uint256 id, string calldata url, string calldata selector, string calldata name, uint256 result) external {
+        require(msg.sender == owner);
+        jobs[id] = Job(url, selector, name, result);
+    }
+
     function getResult(uint256 id) external view returns(uint256) {
         return results[id];
     }
+
+    function getJob(uint256 id) external view returns(string memory url, string memory selector, string memory name, uint256 result) {
+        return (jobs[id].url, jobs[id].selector, jobs[id].name, jobs[id].result);
+    }
+
 }
